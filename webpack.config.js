f9877b98fbff24b905918014dc9dc0f4e8048982
Rayaa <rghirri@gmail.com>
2021-06-20 18:20:28 +0100

    Completed the Testomonials section.

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+        use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
       }
     ]
   }
